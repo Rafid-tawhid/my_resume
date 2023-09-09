fbc03052065cef667e5a11e40331b161f302bcbf
Rafid <rafidtawhid@gmail.com>
2023-09-09 11:27:04 +0600

    my resume basics

---
+++ lib/my_home_page.dart
@@ -0,0 +1,411 @@
+import 'package:flutter/material.dart';
+import 'package:my_resume/project_list.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ContactAndIntro(),
+              ),
+              ClipOval(
+                child: Container(
+                  width: 150.0, // Set the width of the circular image
+                  height: 150.0, // Set the height of the circular image
+                  child: Image.asset('images/photo.jpg'),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    JobExperience(),
+                    SizedBox(height: 10,),
+                    ProjectSection()
+                  ],
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 TrainingSection(),
+                  Text(
+                    'Skill & Tools',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Container(
+                    color: Colors.blueAccent,
+                    width: 120,
+                    height: 2,
+                  ),
+                ],
+              )
+            ],
+          ),
+
+
+        ],
+      ),
+    );
+  }
+
+  Column ProjectSection() {
+    return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'PROJECTS',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Container(
+              color: Colors.blueAccent,
+              width: 120,
+              height: 2,
+            ),
+            SizedBox(height: 4,),
+            ...myProjectList.map((project) => Container(
+              child: ProjectItem(project),
+            ))
+          ],
+        );
+  }
+
+  Column ProjectItem(ProjectInfo project) {
+    return Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(project.id.toString()+'.',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Color(
+                          0xff6e009a)),),
+                      SizedBox(width: 4,),
+                      Text(project.name,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Color(
+                          0xff6e009a))),
+                      SizedBox(width: 2,),
+                      Text('(${project.date})',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Color(
+                          0xff6e009a)))
+                    ],
+                  ),
+                  SizedBox(height: 2,),
+                  Text(project.tools),
+                  SizedBox(height: 2,),
+                  Text(project.details,style: TextStyle(color: Colors.black),),
+                  SizedBox(height: 4,)
+                ],
+              );
+  }
+}
+
+class TrainingSection extends StatelessWidget {
+  const TrainingSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Training',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(
+          height: 2,
+        ),
+        Container(
+          color: Colors.blueAccent,
+          width: 120,
+          height: 2,
+        ),
+        Text(
+          'BTEB Certification',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        Text('Level-4 Certified trainer [2022]',style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),),
+        SizedBox(height: 6,),
+        Text(
+          'Android Application & Development',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        Text('BASIS Institute of Technology & Management (BITM),\nPencilbox-IT, Kawran Bazar, Dhaka.',style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),),
+        SizedBox(height: 6,),
+        Text(
+          'Oracle Certified Professional(OCP)\nJava SE8',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        Text('It Bangla Chottogaram vabon,Topkhana Road,Dhaka.',style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),),
+      ],
+    );
+  }
+}
+
+class ContactAndIntro extends StatelessWidget {
+  const ContactAndIntro({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Tawhidur Rahman Rafid',
+          style:
+              TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.location_on,
+              color: Colors.blueAccent,
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Text(
+              'Kollanpur S.A Khalek Avenue,Road-4 house-60, Dhaka',
+              style: TextStyle(color: Colors.black54),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 6,
+        ),
+        Row(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.call,
+                  color: Colors.blueAccent,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  '01682832598',
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.mail,
+                  color: Colors.blueAccent,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'rafidtawhid@gmail.com',
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 6,
+        ),
+        Row(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.code,
+                  color: Colors.blueAccent,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'github.com/Rafid-tawhid',
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.link_rounded,
+                  color: Colors.blueAccent,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'linkedin.com/in/rafid-tawhid-5a6b75175',
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'I am Passionate to explore the latest technologies and software. Love to create Mobile Application with Flutter. I am a good soldier who can fight in any situation.',
+          style:
+              TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+}
+
+class JobExperience extends StatelessWidget {
+  const JobExperience({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Job Experience',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(
+          height: 2,
+        ),
+        Container(
+          color: Colors.blueAccent,
+          width: 160,
+          height: 2,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'BitBirds Solution (Currently Working)',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        // SizedBox(height: 4,),
+        Text('Flutter Developer',style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),),
+        // SizedBox(height: 4,),
+        Text('September 2021 - December 2022',style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black54,
+        ),),
+        SizedBox(height: 6,),
+        Text(
+          'Pencilbox Training Institute',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        // SizedBox(height: 4,),
+        Text('Assistant Trainer of Mobile Application & Development (SEIP Project-T3)',style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent,
+        ),),
+        // SizedBox(height: 4,),
+        Text('September 2021 - December 2022',style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black54,
+        ),)
+      ],
+    );
+  }
+}
